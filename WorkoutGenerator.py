fd52053e4d1360e673894fdb4130ab4948b992ae
Ivan Zdravkov <ivan.zdravkov@amexis.net>
2017-07-27 13:33:12 +0300

    App renamed.

--- WorkoutGenerator.py
+++ WorkoutGenerator.py
@@ -15,7 +15,7 @@ from oauth2client.file import Storage
 
 SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
 CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'workout planer'
+APPLICATION_NAME = 'Workout Planner'
 
 try:
     flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
